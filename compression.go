bd96b60e92f277f04f0d1e91a60bfe059b445d68
Michael Sparmann <theseven@gmx.net>
2017-03-27 20:21:38 +0200

    Handle fragmented, compressed messages properly

--- compression.go
+++ compression.go
@@ -33,8 +33,9 @@ func decompressNoContextTakeover(r io.Reader) io.ReadCloser {
 		"\x01\x00\x00\xff\xff"
 
 	fr, _ := flateReaderPool.Get().(io.ReadCloser)
-	fr.(flate.Resetter).Reset(io.MultiReader(r, strings.NewReader(tail)), nil)
-	return &flateReadWrapper{fr}
+	reader := io.MultiReader(r, strings.NewReader(tail))
+	fr.(flate.Resetter).Reset(reader, nil)
+	return &flateReadWrapper{fr: fr, reader: reader, rawReader: r}
 }
 
 func isValidCompressionLevel(level int) bool {
@@ -121,6 +122,8 @@ func (w *flateWriteWrapper) Close() error {
 
 type flateReadWrapper struct {
 	fr io.ReadCloser
+	reader io.Reader
+	rawReader io.Reader
 }
 
 func (r *flateReadWrapper) Read(p []byte) (int, error) {
@@ -129,6 +132,11 @@ func (r *flateReadWrapper) Read(p []byte) (int, error) {
 	}
 	n, err := r.fr.Read(p)
 	if err == io.EOF {
+		_, e := r.rawReader.Read(make([]byte, 0))
+		if e == nil {
+			r.fr.(flate.Resetter).Reset(r.reader, nil)
+			return n, nil
+		}
 		// Preemptively place the reader back in the pool. This helps with
 		// scenarios where the application does not call NextReader() soon after
 		// this final read.
